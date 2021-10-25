4a998a4739d51b9d6d6c297ee6ac55a08c6bdd4a
Arsemon4ik <misterarsemon4ik@gmail.com>
2021-10-25 16:21:25 +0300

    lab #4
    add lab 4

---
+++ zvit/labs/lab4/js/js_2.js
@@ -0,0 +1,14 @@
+
+const button = document.getElementById('#');
+
+console.log(button);
+
+button.addEventListener('mouseover',mouse_over);
+button.addEventListener('mouseout',mouse_out);
+
+function mouse_over() {
+    button.style.textDecoration = 'underline';
+}
+function mouse_out() {
+    button.style.textDecoration = 'none';
+}
